ed2b72eedb7a6a79d7745d650603c0a5bb434af5
Shagufta <methwanishagufta@gmail.com>
2020-09-21 21:09:07 +0530

    ..

---
+++ mapper.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+"""mapper.py"""
+
+import sys
+
+# input comes from STDIN (standard input)
+# for product in sys.stdin:
+#     # remove leading and trailing whitespace
+#     line = line.strip()
+#     # split the line into words
+#     words = line.split()
+#     # increase counters
+#     for word in words:
+#         # write the results to STDOUT (standard output);
+#         # what we output here will be the input for the
+#         # Reduce step, i.e. the input for reducer.py
+#         #
+#         # tab-delimited; the trivial word count is 1
+#         print '%s\t%s' % (word, 1)
+
+# select cols as args  
+
+product = {}
+
+ #sys argsargs = []
+
+for line in sys.stdi
+        #pickle, 
+        # printn:
+    if line == "\n":
+        continue
+    else:
+
+        key = keyval[0].strip("")
+                keyval = line.split(": ")
+            product[key] = val        if keyval 
+
+        id
+        
