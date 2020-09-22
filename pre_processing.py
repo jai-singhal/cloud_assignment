fd1435179a69395c9750243af9bc4338def6cf88
Jai Singhal <jaisinghal48@gmail.com>
2020-09-23 01:40:59 +0530

    add pre_processing

---
+++ pre_processing.py
@@ -0,0 +1,77 @@
+import re
+import json
+
+single_value_keys = ["Id", "ASIN", "title", "group", "salesrank"]
+tables = ["similar", "categories", "reviews"]
+RATING_REGEX = r"(\d+\-\d+\-\d+)[\s]*cutomer:[\s]*([0-9A-Z]+)[\s]*rating:[\s]*(\d+)[\s]*votes:[\s]*(\d+)[\s]*helpful:\W+(\d+)"
+
+def getSingletonKeyPair(line):
+    keyval_pair = line.split(":", 2)
+    key = keyval_pair[0].strip("\n \t")
+    val = " ".join(keyval_pair[1:]).strip("\n \t")
+    return key, val
+
+new_line_count = 0
+with open("amazon-meta.txt", "r", encoding="utf-8") as fin:
+    with open("amazon-meta-processed.txt", "w", encoding= "utf-8") as fout:
+        product = {}
+        while True:
+            line = fin.readline()
+            
+            if new_line_count > 2:
+                break
+            
+            if not line.strip():
+                new_line_count += 1  
+                 
+            if line == "\n": 
+                if product:
+                    new_line_count = 0
+                    result = json.dumps(product) 
+                    fout.write(result)
+                    fout.write("\n")
+                    product = {}
+            else:
+                if re.match(r".*:.*", line):
+                    key, val = getSingletonKeyPair(line)
+                    if key in single_value_keys:
+                        product[key] = val
+                    if key == "similar":
+                        product[key] = [
+                            val.strip() for val in val.split(" ")[1:] if len(val) > 0
+                        ]
+                    if key == "categories":
+                        product[key] = []
+                        for i in range(int(val)):
+                            categories = re.findall(r"\|(\w*)\[(\d+)\]", fin.readline().strip())
+                            index = 1
+                            cat_arr = []
+                            for type_, cat_id in categories:
+                                cat_arr.append({
+                                    "index": index,
+                                    "type": type_,
+                                    "cid": cat_id
+                                })
+                                index += 1
+                            product[key].append(cat_arr)  
+                         
+                    if key == "reviews":
+                        # reviews: total: 2  downloaded: 2  avg rating: 5
+                        total, downloaded, avg_rating = re.findall(r"\d*\.\d+|\d+", val.strip())
+                        product["reviews"] = {
+                            "total": total,
+                            "downloaded": downloaded,
+                            "avg_rating": avg_rating,
+                            "review": []
+                        }
+                        for rev in range(int(downloaded)):
+                            review_tup = re.findall(RATING_REGEX, fin.readline().strip())
+                            if not review_tup: continue
+                            review_tup = review_tup[0]
+                            product["reviews"]["review"].append({
+                                "date": review_tup[0],
+                                "user_id": review_tup[1],
+                                "rating": review_tup[2],
+                                "votes": review_tup[3],
+                                "helpful": review_tup[4],
+                            })
--- reducer.py
+++ reducer.py
@@ -1,40 +1,48 @@
 #!/usr/bin/env python
 """reducer.py"""
 
-from operator import itemgetter
+import os
+import pickle
+import re
 import sys
+from operator import itemgetter
 
-current_word = None
-current_count = 0
+current_group = None
+current_group_count = 0
 word = None
+
+# sys args = [func, col1, op, x]
 
 # input comes from STDIN
 for line in sys.stdin:
-    # remove leading and trailing whitespace
-    line = line.strip()
-
+    product = pickle.loads(line.strip(), encoding="UTF-8")
     # parse the input we got from mapper.py
-    word, count = line.split('\t', 1)
+    count = product["count"]
 
-    # convert count (currently a string) to int
-    try:
-        count = int(count)
-    except ValueError:
-        # count was not a number, so silently
-        # ignore/discard this line
-        continue
-
+    # get col1 from sys.arg
+    col1 = sys.argv[1] # list of cols which want to group
+    group = []
+    for col in col1:
+        group.append(product["col"])
+        
     # this IF-switch only works because Hadoop sorts map output
     # by key (here: word) before it is passed to the reducer
-    if current_word == word:
-        current_count += count
+    if current_group == group:
+        current_group_count += count
     else:
-        if current_word:
+        if current_group:
+            product["count"] = current_group_count
+            product_pickled = pickle.dumps(product)
             # write result to STDOUT
-            print '%s\t%s' % (current_word, current_count)
-        current_count = count
-        current_word = word
+            print(product_pickled)
+        current_group_count = count
+        current_group = group
 
-# do not forget to output the last word if needed!
-if current_word == word:
-    print '%s\t%s' % (current_word, current_count)
+# # do not forget to output the last word if needed!
+# if current_group == word:
+#     print(product)
+
+{"njas": sajh ...}\n
+{"njas": sajh ...}\n
+{"njas": sajh ...}\n
+
