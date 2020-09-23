dadafc85f5d926b5c301436b9423d2e9ef8567a2
Jai Singhal <jaisinghal48@gmail.com>
2020-09-23 23:30:54 +0530

    api changes

--- api.py
+++ api.py
@@ -14,17 +14,20 @@ app = FastAPI()
 
 # uvicorn main:app --reload
 query = """
-SELECT abc, def, sum(xyx)
-FROM reviews
-GROUP BY abc, def
-HAVING sum(xyx)>10
+SELECT group, sum(salesrank)
+FROM products
+GROUP BY group
+HAVING sum(salesrank)>10
 """ 
 # {"select": [{"value": "abc"}, {"value": "def"}, {"value": {"sum": "xyx"}}], "from": "reviews", 
 #  "groupby": [{"value": "abc"}, {"value": "def"}], "having": {"gt": [{"sum": "xyx"}, 10]}}
 
 
+class Query(BaseModel):
+    q: str
+
 @app.post("/run/sql")
-async def main(query: str):
+async def main(query: Query):
     def get_cmd():
         MAP_REDUCE_CMD = """
             hadoop jar /usr/lib/hadoop-mapreduce/hadoop-streaming.jar 
@@ -35,44 +38,50 @@ async def main(query: str):
             -input data/amazon-meta-processed.txt
             -output /user/hduser/gutenberg-output/r
         """
-    def get_select_cols(select):
-        select_cols = []
+    def get_select_cols(select_cols):
+        select_cols_parsed = []
         aggregate_fns = {}
-        for k, v in select.items():
-            if isinstance(v, dict):
-                aggregate_fns = {
-                    "fun": v.keys()[0],
-                    "col": v.values()[0],
-                }
-            else:
-                select_cols.append(v)
-        return select_cols, aggregate_fns        
+        print(select_cols)
+        for s_col in select_cols:
+            for k, v in s_col.items():
+                if isinstance(v, dict):
+                    for v1, v2 in v.items():
+                        aggregate_fns = {
+                            "fun": v1,
+                            "col": v2,
+                        }
+                else:
+                    select_cols_parsed.append(v)
+        return (select_cols_parsed, aggregate_fns)        
         
     def get_mapper_test_cmd(parsed):
+        print(parsed["select"])
         select_cols, agg_fn = get_select_cols(parsed["select"])
-        select_cols_pickle = binascii.hexlify(pickle.dumps(select_cols))
-        return f"""cat data/test.txt |
-         python3 mapper.py {select_cols_pickle} {parsed["from"]} {agg_fn["col"]}
-        """
+        select_cols_pickle = binascii.hexlify(pickle.dumps(select_cols)).decode()
+        return f"cat data/test.txt", f"""python3 mapper.py {select_cols_pickle} {parsed["from"]} {agg_fn["col"]}"""
         
     def get_reducer_test_cmd(parsed, map_out):
         select_cols, agg_fn = get_select_cols(parsed["select"])
         X = parsed["having"]["gt"][1]
-        return f"""echo {map_out} |
-         python3 reducer.py {agg_fn["fun"]} {X}
-        """
+        return f"""echo {map_out}""",  f"""python3 reducer.py {agg_fn["fun"]} {X}"""
         
-    parsed = json.dumps(parse(query))
+    parsed = parse(query.q)
+    mapper_cmd1, mapper_cmd2  = get_mapper_test_cmd(parsed)
+    map_process_temp = Popen(mapper_cmd1.split(" "), stdin=PIPE, stdout=PIPE, stderr=PIPE)
+    map_pipe_process = Popen(mapper_cmd2.split(" "), stdin=map_process_temp.stdout, stdout=PIPE)
+    map_process_temp.stdout.close() 
+
+    mapper_output, err = map_pipe_process.communicate()
+    rc_m = map_pipe_process.returncode
     
-    mapper_cmd = get_mapper_test_cmd(parsed)
-    map_process = Popen(mapper_cmd, stdin=PIPE, stdout=PIPE, stderr=PIPE)
-    mapper_output, err = map_process.communicate()
-    rc_m = map_process.returncode
+    ## reducer
+    reducer_cmd1, reducer_cmd2 = get_reducer_test_cmd(parsed, mapper_output)
+    reducer_process_tmp = Popen(reducer_cmd1.split(" "), stdin=PIPE, stdout=PIPE, stderr=PIPE)
+    reducer_pipe_process = Popen(reducer_cmd2.split(" "), stdin=reducer_process_tmp.stdout, stdout=PIPE)
     
-    reducer_cmd = get_reducer_test_cmd(parsed, mapper_output)
-    reducer_process = Popen(reducer_cmd, stdin=PIPE, stdout=PIPE, stderr=PIPE)
-    reducer_output, err = reducer_process.communicate()
-    rc_r = reducer_process.returncode
+    reducer_process_tmp.stdout.close() 
+    reducer_output, err = reducer_pipe_process.communicate()
+    rc_r = reducer_pipe_process.returncode
     
     return {
         "reducer_output": reducer_output, 
