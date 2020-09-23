17f5bc2eee020149f713fefaf73a33b62df07e8f
Jai Singhal <jaisinghal48@gmail.com>
2020-09-23 22:49:55 +0530

    add api

--- api.py
+++ api.py
@@ -1,8 +1,13 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
-import re
+import binascii
 import json
 import os
+import pickle
+import re
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+from subprocess import Popen, PIPE
+
 from moz_sql_parser import parse
 
 app = FastAPI()
@@ -14,22 +19,62 @@ FROM reviews
 GROUP BY abc, def
 HAVING sum(xyx)>10
 """ 
-# sql_regex = re.compile(
-#     ("""SELECT\s*([a-zA-Z0-9]+),*\s*([a-zA-Z0-9]*),*\s*(([A-Za-z]+)\(([a-zA-Z0-9]+)\))\s*\n*"""
-#     """\s*FROM\s*([a-zA-Z]+)\s*\n*"""
-#     """GROUP\s*BY\s*([a-zA-Z0-9]+),*\s*([a-zA-Z0-9]+)*"""
-#     """HAVING\s*([A-Za-z]+)\(([a-zA-Z0-9]+)\)>(\d+)"""
-# ), re.IGNORECASE)
+# {"select": [{"value": "abc"}, {"value": "def"}, {"value": {"sum": "xyx"}}], "from": "reviews", 
+#  "groupby": [{"value": "abc"}, {"value": "def"}], "having": {"gt": [{"sum": "xyx"}, 10]}}
 
-def queryParam(sqlQuery):
-    # sqlQuery = sqlQuery.replace("\n", " ")
-    # return re.findall(sql_regex, query)
-    parsed = json.dumps(parse(sqlQuery))
-    print(parsed)
-
-print(queryParam(query))
 
 @app.post("/run/sql")
 async def main(query: str):
-    print(query)
-    return {"message": "Hello World"}
+    def get_cmd():
+        MAP_REDUCE_CMD = """
+            hadoop jar /usr/lib/hadoop-mapreduce/hadoop-streaming.jar 
+            -file mapper.py 
+            -mapper /home/cloudera/Downloads/tmp/mapper.py 
+            -file reducer.py 
+            -reducer /home/cloudera/Downloads/tmp/reducer.py 
+            -input data/amazon-meta-processed.txt
+            -output /user/hduser/gutenberg-output/r
+        """
+    def get_select_cols(select):
+        select_cols = []
+        aggregate_fns = {}
+        for k, v in select.items():
+            if isinstance(v, dict):
+                aggregate_fns = {
+                    "fun": v.keys()[0],
+                    "col": v.values()[0],
+                }
+            else:
+                select_cols.append(v)
+        return select_cols, aggregate_fns        
+        
+    def get_mapper_test_cmd(parsed):
+        select_cols, agg_fn = get_select_cols(parsed["select"])
+        select_cols_pickle = binascii.hexlify(pickle.dumps(select_cols))
+        return f"""cat data/test.txt |
+         python3 mapper.py {select_cols_pickle} {parsed["from"]} {agg_fn["col"]}
+        """
+        
+    def get_reducer_test_cmd(parsed, map_out):
+        select_cols, agg_fn = get_select_cols(parsed["select"])
+        X = parsed["having"]["gt"][1]
+        return f"""echo {map_out} |
+         python3 reducer.py {agg_fn["fun"]} {X}
+        """
+        
+    parsed = json.dumps(parse(query))
+    
+    mapper_cmd = get_mapper_test_cmd(parsed)
+    map_process = Popen(mapper_cmd, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+    mapper_output, err = map_process.communicate()
+    rc_m = map_process.returncode
+    
+    reducer_cmd = get_reducer_test_cmd(parsed, mapper_output)
+    reducer_process = Popen(reducer_cmd, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+    reducer_output, err = reducer_process.communicate()
+    rc_r = reducer_process.returncode
+    
+    return {
+        "reducer_output": reducer_output, 
+        "mapper_output": mapper_output
+    }
