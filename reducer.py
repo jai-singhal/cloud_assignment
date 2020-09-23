2cc7e0c5c100a380b02b499964b741fc2ab9e8ed
Shagufta <methwanishagufta@gmail.com>
2020-09-23 14:52:56 +0530

    Reducer.py for the assignment

--- reducer.py
+++ reducer.py
@@ -4,37 +4,145 @@
 from operator import itemgetter
 import sys
 
-current_word = None
-current_count = 0
-word = None
+current_columns = None
+minval = -sys.maxsize - 1
+minval = sys.maxsize 
+current_max = minval
+current_min = maxval
+columns = None
+fun = 'MAX' #to be changed later
+x = 5 #to be changed later
 
+
+if fun=='MAX':
 # input comes from STDIN
-for line in sys.stdin:
-    # remove leading and trailing whitespace
-    line = line.strip()
+    for line in sys.stdin:
+        # remove leading and trailing whitespace
+        line = line.strip()
 
-    # parse the input we got from mapper.py
-    word, count = line.split('\t', 1)
+        columns, column1 = line.split('\t', 1)
 
-    # convert count (currently a string) to int
-    try:
-        count = int(count)
-    except ValueError:
-        # count was not a number, so silently
-        # ignore/discard this line
-        continue
+        try:
+            column1 = int(column1)
+        except ValueError:
+            continue
 
-    # this IF-switch only works because Hadoop sorts map output
-    # by key (here: word) before it is passed to the reducer
-    if current_word == word:
-        current_count += count
-    else:
-        if current_word:
-            # write result to STDOUT
-            print '%s\t%s' % (current_word, current_count)
-        current_count = count
-        current_word = word
+        # this IF-switch only works because Hadoop sorts map output
+        # by key (here: word) before it is passed to the reducer
+        if not current_columns:
+            current_columns=columns
 
-# do not forget to output the last word if needed!
-if current_word == word:
-    print '%s\t%s' % (current_word, current_count)
+        if current_columns == cols:
+            current_max= max(current_max, column1)
+
+        else:
+            if current_max > x:
+                result = [current_columns, current_max]
+                print("\t".join(str(v) for v in result))
+
+            current_max = minval
+            current_columns = columns
+
+    # do not forget to output the last word if needed!
+    if current_columns == columns:
+        if current_max > x:
+                result = [current_columns, current_max]
+                print("\t".join(str(v) for v in result))
+
+
+elif fun=='MIN':
+
+    for line in sys.stdin:
+        line = line.strip()
+        columns, column1 = line.split('\t', 1)
+
+        try:
+            column1 = int(column1)
+        except ValueError:
+            continue
+
+        if not current_columns:
+            current_columns=columns
+
+        if current_columns == cols:
+            current_min= min(current_min, column1)
+
+        else:
+            if current_min > x:
+                result = [current_columns, current_min]
+                print("\t".join(str(v) for v in result))
+
+            current_min = maxval
+            current_columns = columns
+
+    # do not forget to output the last word if needed!
+    if current_columns == columns:
+        if current_min > x:
+                result = [current_columns, current_min]
+                print("\t".join(str(v) for v in result))
+
+elif fun=='SUM':
+    sumval=0
+
+    for line in sys.stdin:
+        line = line.strip()
+        columns, column1 = line.split('\t', 1)
+
+        try:
+            column1 = int(column1)
+        except ValueError:
+            continue
+
+        if not current_columns:
+            current_columns=columns
+
+        if current_columns == cols:
+            sumval= sumval+column1
+
+        else:
+            if sumval > x:
+                result = [current_columns, sumval]
+                print("\t".join(str(v) for v in result))
+
+            sumval = 0
+            current_columns = columns
+
+    # do not forget to output the last word if needed!
+    if current_columns == columns:
+        if sumval > x:
+                result = [current_columns, sumval]
+                print("\t".join(str(v) for v in result))
+
+elif fun=='COUNT':
+    sumval=0
+
+    for line in sys.stdin:
+        line = line.strip()
+        columns, column1 = line.split('\t', 1)
+
+        try:
+            column1 = int(column1)
+        except ValueError:
+            continue
+
+        if not current_columns:
+            current_columns=columns
+
+        if current_columns == cols:
+            sumval= sumval +1
+
+        else:
+            if sumval > x:
+                result = [current_columns, sumval]
+                print("\t".join(str(v) for v in result))
+
+            sumval = 0
+            current_columns = columns
+
+    # do not forget to output the last word if needed!
+    if current_columns == columns:
+        if sumval > x:
+                result = [current_columns, sumval]
+                print("\t".join(str(v) for v in result))
+
+    
