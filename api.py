e3f9caee0255ed16ed8c4b4b445f733c9af08eb9
Jai Singhal <jaisinghal48@gmail.com>
2020-09-28 15:18:05 +0530

    update api

--- api.py
+++ api.py
@@ -37,13 +37,15 @@ import random
 async def spark_post(query: Query):
     parsed = parse(query.q)
     print(parsed)
+    
     sc = SparkContext("local","PySpark sql run")
     data = sc.textFile("data/test.txt")
     m = Mapper(*get_mapper_args(parsed))
-    # (a, b)
+    
+    func, op, Y = get_reducer_args(parsed)
+    
     sparkmapp = data.map(lambda line: m.run_spark(line)).reduceByKey(lambda a, b: a+b)
     res = sparkmapp.collect()
-    # wordCounts = data.map()
     return {
         "out": res,
     }
@@ -61,46 +63,51 @@ async def map_reduce_post(query: Query):
             -input data/amazon-meta-processed.txt
             -output /user/hduser/gutenberg-output/r
         """
-   
+    
+    def run_mapper_process():
+        mapper_cmd = ["python3", "mapper.py"]
+        for arg in get_mapper_args(parsed): mapper_cmd.append(arg)
+
+        map_process_temp = Popen(["cat", "data/test.txt"], stdin=PIPE, stdout=PIPE, stderr=PIPE)
+        print(map_process_temp.stdout)
+        map_pipe_process = Popen(mapper_cmd, stdin=map_process_temp.stdout, stdout=PIPE, stderr=PIPE)
+
+        mapper_output, err = map_pipe_process.communicate()
+        map_process_temp.stdout.close() 
+
+        rc_m = map_pipe_process.returncode
+        print("Error in mapper: ", err.decode(), 'return code: ', rc_m)
+        return mapper_output
+    
+    def run_reducer_process():
+        reducer_cmd = ["python3", "reducer.py"] 
+        for arg in get_reducer_args(parsed):
+            reducer_cmd.append(arg)
+
+        reducer_temp_process = Popen(['cat', 'reducer_inp.txt'], stdin=PIPE, stdout=PIPE, stderr=PIPE)
+        reducer_pipe_process = Popen(reducer_cmd, stdin=reducer_temp_process.stdout, stdout=PIPE)
+        reducer_output, err = reducer_pipe_process.communicate()
+        reducer_temp_process.stdout.close() 
+        print("Error in reducer: ", err)
+        rc_r = reducer_pipe_process.returncode
+        return reducer_output
+
+
     parsed = parse(query.q)
     print(parsed)
     ## mapper
-    mapper_cmd = ["python3", "mapper.py"]
-    for arg in get_mapper_args(parsed): mapper_cmd.append(arg)
-    
-    map_process_temp = Popen(["cat", "data/test.txt"], stdin=PIPE, stdout=PIPE, stderr=PIPE)
-    print(map_process_temp.stdout)
-    map_pipe_process = Popen(mapper_cmd, stdin=map_process_temp.stdout, stdout=PIPE, stderr=PIPE)
-
-    mapper_output, err = map_pipe_process.communicate()
-    map_process_temp.stdout.close() 
-    
-    rc_m = map_pipe_process.returncode
-    print("Error in mapper: ", err.decode(), 'return code: ', rc_m)
-    
+    mapper_output = run_mapper_process()
     mapper_output_decoded = mapper_output.decode()
-    print("mapper out: ", mapper_output_decoded)
-    
-    reducer_in = open("reducer_inp.txt", "w")
-    for line in mapper_output_decoded.split("\n"):
-        if line:
-            reducer_in.write(line)
-            reducer_in.write("\n")
-    
-    reducer_in.close()
+    with open("reducer_inp.txt", "w") as reducer_in:
+        for line in mapper_output_decoded.split("\n"):
+            if line:
+                reducer_in.write(line)
+                reducer_in.write("\n")
           
     # reducer
-    reducer_cmd = get_reducer_test_cmd(parsed, mapper_output)
-    reducer_temp_process = Popen(['cat', 'reducer_inp.txt'], stdin=PIPE, stdout=PIPE, stderr=PIPE)
-    reducer_pipe_process = Popen(reducer_cmd.split(" "), stdin=reducer_temp_process.stdout, stdout=PIPE)
-    reducer_output, err = reducer_pipe_process.communicate()
-    reducer_temp_process.stdout.close() 
-    
-    print("Error in reducer: ", err)
-    print("reducer out: ", reducer_output.decode())
-    rc_r = reducer_pipe_process.returncode
-    
+    reducer_output = run_reducer_process()
+    reducer_output_decoded = reducer_output.decode()
     return {
         "mapper_output": mapper_output_decoded,
-        "reducer_output": reducer_output.decode()
+        "reducer_output": reducer_output_decoded
     }
